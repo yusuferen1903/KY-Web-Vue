40a7e2d613f07320aaf50f7d0856d737e23f204e
yusuferen1903 <yusuferenuneyler@gmail.com>
2019-11-18 00:15:38 +0300

    Oneri ve Şikayet sayfasının içeriği eklendi

--- src/components/onerisikayet.vue
+++ src/components/onerisikayet.vue
@@ -1,3 +1,419 @@
 <template>
-  <div></div>
+  <div>
+    <div class="govde">
+      <div class="iletisim">
+        <div class="kolon1">
+          <h3 class="iletisimyazisi">KÖFTECİ YUSUF İLETİŞİM</h3>
+          <div class="adresbilgileri">
+            <div class="iletisim">
+              <div class="iletisim1">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="map-marker-alt"
+                  class="iletisim2"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"
+                  />
+                </svg>
+
+                <div class="iletisim3">Kurtuluş Mah. Terziler Yolu No:4/1 Yenişehir Bursa</div>
+              </div>
+              <div class="iletisim4">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="phone"
+                  class="iletisim5"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"
+                  />
+                </svg>
+
+                <a href="tel:444 61 62">444 61 62</a>
+              </div>
+              <div class="iletisim4">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="phone"
+                  class="iletisim5"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"
+                  />
+                </svg>
+
+                <a href="tel:0224 773 6261">0224 773 6261</a>
+              </div>
+              <div class="iletisim1">
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="envelope"
+                  class="iletisim2"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"
+                  />
+                </svg>
+
+                <a
+                  href="mailto:musterihizmetleri@kofteciyusuf.com.tr"
+                  class="d-block ml-4 fs-13"
+                >musterihizmetleri@kofteciyusuf.com.tr</a>
+              </div>
+              <div class="iletisim1">
+                <a href="subelerimiz.html" class="kirmizibuton">
+                  <span>Tüm Şubelerimiz İçin Tıklayınız</span>
+                </a>
+              </div>
+            </div>
+            <div class="iletisim1">
+              <a href="kiralik-isyeri-onerisi.html" class="turuncubuton">
+                <span>Kiralık Yer Önermek İçin Tıklayınız</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="kolon2">
+          <h3 class="iletisimyazisi">İLETİŞİM FORMU</h3>
+          <form
+            action="javascript:void(0);"
+            method="post"
+            class="form form-contact security-control"
+            id="contactForm"
+          >
+            <div class="col-sm-6 w350">
+              <input
+                type="text"
+                name="name_surname"
+                placeholder="Adınız Soyadınız *"
+                class="form-input mb-25"
+                required
+              />
+            </div>
+            <div class="col-sm-6 w350">
+              <input
+                type="email"
+                name="email"
+                placeholder="E-Posta Adresiniz *"
+                class="form-input mb-25"
+                required
+              />
+            </div>
+            <div class="col-sm-6 w350">
+              <input
+                type="tel"
+                name="telephone"
+                placeholder="Telefon Numaranız *"
+                class="form-input mb-25"
+              />
+            </div>
+            <div class="col-sm-6 w350">
+              <select name="why" id="cause" class="form-input mb-25" style="width: 350px;">
+                <option>İletişim Sebebiniz?</option>
+                <option>Öneri</option>
+                <option>İstek</option>
+                <option>Şikayet</option>
+                <option>Teşekkür</option>
+              </select>
+            </div>
+            <div class="col-sm-12" style="width: 350px;">
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Mesajınız *"
+                rows="4"
+                class="form-input mb-25"
+              ></textarea>
+            </div>
+            <div class="col-sm-12">
+              <div
+                class="g-recaptcha rendered"
+                data-sitekey="6Ldl7bwUAAAAAORKMciuTm6UZTqtL5KEl4QoemAV"
+                data-callback="contactForm"
+                data-size="invisible"
+                data-recaptchawidgetid="0"
+              >
+                <div
+                  class="grecaptcha-badge"
+                  data-style="bottomright"
+                  style="width: 256px; height: 60px; position: fixed; visibility: hidden; display: block; transition: right 0.3s ease 0s; bottom: 14px; right: -186px; box-shadow: gray 0px 0px 5px; border-radius: 2px; overflow: hidden;"
+                >
+                  <div class="grecaptcha-logo">
+                    <iframe
+                      src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6Ldl7bwUAAAAAORKMciuTm6UZTqtL5KEl4QoemAV&amp;co=aHR0cHM6Ly9rb2Z0ZWNpeXVzdWYuY29tOjQ0Mw..&amp;hl=tr&amp;v=75nbHAdFrusJCwoMVGTXoHoM&amp;size=invisible&amp;cb=lms3sn8jodgl"
+                      width="256"
+                      height="60"
+                      role="presentation"
+                      name="a-3x0o4o9pghdc"
+                      frameborder="0"
+                      scrolling="no"
+                      sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"
+                    ></iframe>
+                  </div>
+                  <div class="grecaptcha-error"></div>
+                  <textarea
+                    id="g-recaptcha-response"
+                    name="g-recaptcha-response"
+                    class="g-recaptcha-response"
+                    style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"
+                  ></textarea>
+                </div>
+              </div>
+
+              <button type="submit" class="gonderbuton">GÖNDER</button>
+            </div>
+            <div class="col-sm-12">
+              <div class="form-result mt-4"></div>
+            </div>
+          </form>
+        </div>
+        <div class="col-sm-12 mt-40">
+          <div class="embed-responsive embed-responsive-21by9 border-radius-10">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12177.592235831615!2d29.661666!3d40.266907!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xec4ec95fbb3b74ff!2sK%C3%B6fteci+Yusuf!5e0!3m2!1str!2str!4v1520072464809"
+              width="1200"
+              height="500"
+              frameborder="0"
+              style="border:0"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.iletisim {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+}
+
+.kolon1 {
+  flex: 0 0 33.333333%;
+  max-width: 33.333333%;
+  position: relative;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+.iletisimyazisi {
+  position: relative;
+  font-size: 22px;
+  margin-bottom: 30px;
+  margin-top: 39px;
+  font-family: sans-serif;
+}
+
+.iletisimyazisi::after {
+  content: "";
+  display: block;
+  width: 87px;
+  height: 2px;
+  background-color: #fa9628;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+}
+
+.col-sm-6 {
+  flex: 0 0 50%;
+  max-width: 50%;
+  font-family: sans-serif;
+  width: 350px !important;
+}
+
+.col-sm-12 {
+  flex: 0 0 100%;
+  max-width: 100%;
+  font-family: sans-serif;
+}
+
+.form {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+}
+
+.govde {
+  margin-bottom: 3rem !important;
+  max-width: 1140px;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.iletisim svg {
+  overflow: hidden;
+  vertical-align: middle;
+}
+
+.adresbilgileri {
+  padding: 30px;
+  background-color: #fbfbfb;
+}
+
+a {
+  color: #fa9628;
+  text-decoration: none;
+  background-color: transparent;
+}
+
+.iletisim1 {
+  margin-bottom: 30px;
+  flex: 0 0 100%;
+  max-width: 100%;
+}
+
+.iletisim2 {
+  float: left;
+  width: 15px;
+  color: #fa9628;
+  margin-right: 0.5rem !important;
+}
+
+.iletisim3 {
+  margin-left: 1.5rem !important;
+  font-family: sans-serif;
+  color: #55494e;
+}
+
+.iletisim4 {
+  margin-bottom: 30px;
+  flex: 0 0 50%;
+  max-width: 50%;
+}
+
+.iletisim5 {
+  width: 15px;
+  color: #fa9628;
+  margin-right: 0.5rem !important;
+}
+
+.kirmizibuton {
+  background-color: #cc021b !important;
+  border: none;
+  background: none;
+  box-shadow: none;
+  text-decoration: none;
+  display: inline-block;
+  padding: 10px;
+  position: relative;
+  color: #fff;
+  transition: all 0.2s;
+  border-radius: 10px;
+  text-align: center;
+  display: block !important;
+  font-family: sans-serif;
+}
+
+.kirmizibuton:hover {
+  background-color: #e5021e !important;
+}
+
+.turuncubuton {
+  background-color: #fa9628 !important;
+  border: none;
+  background: none;
+  box-shadow: none;
+  text-decoration: none;
+  display: inline-block;
+  padding: 10px;
+  position: relative;
+  color: #fff;
+  transition: all 0.2s;
+  border-radius: 10px;
+  text-align: center;
+  display: block !important;
+  width: 343px;
+  margin-left: -17px;
+  font-family: sans-serif;
+}
+
+.turuncubuton:hover {
+  background-color: #fba241 !important;
+}
+
+.kolon2 {
+  flex: 0 0 57.666667%;
+  max-width: 57.666667%;
+  position: relative;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+.form-input {
+  display: block;
+  width: 100%;
+  border: 1px solid #e3e3e3;
+  padding: 8px 10px;
+  font-weight: 300;
+  outline: none;
+  border-radius: 10px;
+  width: 310px;
+}
+
+.mb-25 {
+  margin-bottom: 25px;
+}
+
+.gonderbuton {
+  border-radius: 10px;
+  cursor: pointer;
+  background-color: #fa9628 !important;
+  border: none;
+  height: 100%;
+  background: none;
+  box-shadow: none;
+  text-decoration: none;
+  display: inline-block;
+  padding: 10px;
+  position: relative;
+  color: #fff;
+  transition: all 0.2s;
+  padding-left: 3rem !important;
+  padding-right: 3rem !important;
+  font-family: sans-serif;
+}
+
+.gonderbuton:hover {
+  background-color: #fba241 !important;
+}
+
+.w350 {
+  width: 350px !important;
+}
+</style>
