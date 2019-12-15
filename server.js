984f486195a7960d2ad739fbca929896d92fe253
yusuferen1903 <yusuferenuneyler@gmail.com>
2019-12-15 15:52:44 +0300

    Cig ürünler için post ve dlete işlmleri olşturuldu

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     app = express();
 fs = require('fs');
-
+var bodyParser = require("body-parser")
+var _ = require("underscore");
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
 
     //res.send('ürünleri listeler');
@@ -12,6 +14,17 @@ app.get('/', function (req, res) {
 
 
 })
+
+app.post('/', function (req, res) {
+
+    //res.send('ürünleri listeler');
+    let body = _.pick(req.body, "id", "urunismi", "Fiyat", "Resim");
+
+    res.send(body);
+})
+
+
+
 app.get('/CigUrunler', function (req, res) {
 
     //res.send('ürünleri listeler');
@@ -97,12 +110,46 @@ app.get('/ekle', function (req, res) {
         });
     })
 })
+app.delete('/CigUrunler', function (req, res) {
+    //silme işlemi için silmek istediğiniz ürün id ismini delete datadan sonra giriniz
+    fs.readFile('CigUrunler.json', 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        //Silinmesi İstenen Ürünün Adı Buraya Girilmeli
+        delete data["AtEti4"]
+        console.log(data);
+        res.end("Urun Silindi")
+        fs.writeFile('CigUrunler.json', JSON.stringify(data), function (err) {
+
+        });
+    })
+})
+//Ciğ ürünler için post işlemi
+app.post('/CigUrunler', function (req, res) {
+    //silme işlemi için silmek istediğiniz ürün id ismini delete datadan sonra giriniz
+    var yeniurun = {
+        "AtEti4": {
+            "urunismi": "At",
+            "Fiyat": "10TL/200GR",
+            "Resim": "https://kofteciyusuf.com/uploads/pictures/thumb_yusuf-kofte_27.07.2019_08_16_05.jpg"
+        }
+    };
+    //hangi json belgesine eklenmek isteneceği aşağıdaki tırnak içindeki datalara girilmelidir
+    fs.readFile('CigUrunler.json', 'utf8', function (err, data) {
+        data = JSON.parse(data);
+        data["AtEti4"] = yeniurun["AtEti4"]
+        console.log(data);
+        res.end("Urun Eklendi")
+        fs.writeFile('CigUrunler.json', JSON.stringify(data), function (err) {
+
+        });
+    })
+})
 app.get('/sil', function (req, res) {
     //silme işlemi için silmek istediğiniz ürün id ismini delete datadan sonra giriniz
     fs.readFile('CigUrunler.json', 'utf8', function (err, data) {
         data = JSON.parse(data);
         //Silinmesi İstenen Ürünün Adı Buraya Girilmeli
-        delete data[""]
+        delete data["AtEti4"]
         console.log(data);
         res.end("Urun Silindi")
         fs.writeFile('CigUrunler.json', JSON.stringify(data), function (err) {
@@ -148,13 +195,13 @@ app.get('/urunbul', function (req, res) {
     fs.readFile('tumurunler.json', 'utf8', function (err, data) {
         data = JSON.parse(data);
         //Bulunması  İstenen Ürünün Adı Buraya Girilmeli
-        console.log(data["Sucuk"]);
-        res.end(JSON.stringify(data["Sucuk"]))
+        console.log(data["DanaBonfile"]);
+        res.end(JSON.stringify(data["DanaBonfile"]))
     })
 
 })
-
+//sunucunun hangi portta çalışıtığını gösterir
 var server = app.listen(3000, function () {
-    console.log('sunucu calisiyor');
+    console.log('Sunucu Calisiyor');
 });
 
